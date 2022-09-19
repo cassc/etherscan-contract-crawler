9762082aa1888393d62094e46ca734b15190b68c
Chen <chencassc@gmail.com>
2022-09-19 13:10:43 +0800

    update contracts

---
+++ contracts/0x2B9F771012E83368168A3E6c92c7421b333cB671_DelegatableRelay/01_06_DelegatableRelay.sol
@@ -0,0 +1,144 @@
+// SPDe-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+// import "hardhat/console.sol";
+import {EIP712DOMAIN_TYPEHASH} from "./TypesAndDecoders.sol";
+import {Delegation, Invocation, Invocations, SignedInvocation, SignedDelegation} from "./CaveatEnforcer.sol";
+import {DelegatableRelayCore} from "./DelegatableRelayCore.sol";
+import {IDelegatable} from "./interfaces/IDelegatable.sol";
+
+contract DelegatableRelay is IDelegatable, DelegatableRelayCore {
+    /// @notice The hash of the domain separator used in the EIP712 domain hash.
+    bytes32 public immutable domainHash;
+
+    /**
+     * @notice Delegatable Relay Constructor
+     */
+    constructor() {
+        domainHash = getEIP712DomainHash(
+            "DelegatableRelay",
+            "1",
+            block.chainid,
+            address(this)
+        );
+    }
+
+    /* ===================================================================================== */
+    /* External Functions                                                                    */
+    /* ===================================================================================== */
+
+    /// @inheritdoc IDelegatable
+    function getDelegationTypedDataHash(Delegation memory delegation)
+        public
+        view
+        returns (bytes32)
+    {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainHash,
+                GET_DELEGATION_PACKETHASH(delegation)
+            )
+        );
+        return digest;
+    }
+
+    /// @inheritdoc IDelegatable
+    function getInvocationsTypedDataHash(Invocations memory invocations)
+        public
+        view
+        returns (bytes32)
+    {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainHash,
+                GET_INVOCATIONS_PACKETHASH(invocations)
+            )
+        );
+        return digest;
+    }
+
+    function getEIP712DomainHash(
+        string memory contractName,
+        string memory version,
+        uint256 chainId,
+        address verifyingContract
+    ) public pure returns (bytes32) {
+        bytes memory encoded = abi.encode(
+            EIP712DOMAIN_TYPEHASH,
+            keccak256(bytes(contractName)),
+            keccak256(bytes(version)),
+            chainId,
+            verifyingContract
+        );
+        return keccak256(encoded);
+    }
+
+    function verifyDelegationSignature(SignedDelegation memory signedDelegation)
+        public
+        view
+        virtual
+        override(IDelegatable, DelegatableRelayCore)
+        returns (address)
+    {
+        Delegation memory delegation = signedDelegation.delegation;
+        bytes32 sigHash = getDelegationTypedDataHash(delegation);
+        address recoveredSignatureSigner = recover(
+            sigHash,
+            signedDelegation.signature
+        );
+        return recoveredSignatureSigner;
+    }
+
+    function verifyInvocationSignature(SignedInvocation memory signedInvocation)
+        public
+        view
+        returns (address)
+    {
+        bytes32 sigHash = getInvocationsTypedDataHash(
+            signedInvocation.invocations
+        );
+        address recoveredSignatureSigner = recover(
+            sigHash,
+            signedInvocation.signature
+        );
+        return recoveredSignatureSigner;
+    }
+
+    // --------------------------------------
+    // WRITES
+    // --------------------------------------
+
+    /// @inheritdoc IDelegatable
+    function contractInvoke(Invocation[] calldata batch)
+        external
+        override
+        returns (bool)
+    {
+        return _invoke(batch, msg.sender);
+    }
+
+    /// @inheritdoc IDelegatable
+    function invoke(SignedInvocation[] calldata signedInvocations)
+        external
+        override
+        returns (bool success)
+    {
+        for (uint256 i = 0; i < signedInvocations.length; i++) {
+            SignedInvocation calldata signedInvocation = signedInvocations[i];
+            address invocationSigner = verifyInvocationSignature(
+                signedInvocation
+            );
+            _enforceReplayProtection(
+                invocationSigner,
+                signedInvocations[i].invocations.replayProtection
+            );
+            _invoke(signedInvocation.invocations.batch, invocationSigner);
+        }
+    }
+
+    /* ===================================================================================== */
+    /* Internal Functions                                                                    */
+    /* ===================================================================================== */
+}
---
+++ contracts/0x2B9F771012E83368168A3E6c92c7421b333cB671_DelegatableRelay/05_06_IDelegatable.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import "../TypesAndDecoders.sol";
+
+interface IDelegatable {
+    /**
+     * @notice Allows a smart contract to submit a batch of invocations for processing, allowing itself to be the delegate.
+     * @param batch Invocation[] - The batch of invocations to process.
+     * @return success bool - Whether the batch of invocations was successfully processed.
+     */
+    function contractInvoke(Invocation[] calldata batch)
+        external
+        returns (bool);
+
+    /**
+     * @notice Allows anyone to submit a batch of signed invocations for processing.
+     * @param signedInvocations SignedInvocation[] - The batch of signed invocations to process.
+     * @return success bool - Whether the batch of invocations was successfully processed.
+     */
+    function invoke(SignedInvocation[] calldata signedInvocations)
+        external
+        returns (bool success);
+
+    /**
+     * @notice Returns the typehash for this contract's delegation signatures.
+     * @param delegation Delegation - The delegation to get the type of
+     * @return bytes32 - The type of the delegation
+     */
+    function getDelegationTypedDataHash(Delegation memory delegation)
+        external
+        view
+        returns (bytes32);
+
+    /**
+     * @notice Returns the typehash for this contract's invocation signatures.
+     * @param invocations Invocations
+     * @return bytes32 - The type of the Invocations
+     */
+    function getInvocationsTypedDataHash(Invocations memory invocations)
+        external
+        view
+        returns (bytes32);
+
+    function getEIP712DomainHash(
+        string memory contractName,
+        string memory version,
+        uint256 chainId,
+        address verifyingContract
+    ) external pure returns (bytes32);
+
+    /**
+     * @notice Verifies that the given invocation is valid.
+     * @param signedInvocation - The signed invocation to verify
+     * @return address - The address of the account authorizing this invocation to act on its behalf.
+     */
+    function verifyInvocationSignature(SignedInvocation memory signedInvocation)
+        external
+        view
+        returns (address);
+
+    /**
+     * @notice Verifies that the given delegation is valid.
+     * @param signedDelegation - The delegation to verify
+     * @return address - The address of the account authorizing this delegation to act on its behalf.
+     */
+    function verifyDelegationSignature(SignedDelegation memory signedDelegation)
+        external
+        view
+        returns (address);
+}
