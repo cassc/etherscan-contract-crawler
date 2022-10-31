019aed5625e5cc2d39705c5f7aaa4d1f54e987dc
Chen <chencassc@gmail.com>
2022-10-31 13:25:57 +0800

    update contracts

---
+++ bsc_contracts/0x0401116ECC03a3d51Ca19cB1350460C55c6281f0_LimitedRhinoToken/02_16_RhinoTokenStructs.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.16;
+
+    struct List {
+        uint256 amount;
+        uint256 reward;
+    }
---
+++ bsc_contracts/0xb463f85621cCAC9f7178eA72CAAA27941849f738_FarmGenerator/06_21_IERCBurn.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+interface IERCBurn {
+  function burn(uint256 _amount) external;
+  function approve(address spender, uint256 amount) external returns (bool);
+  function allowance(address owner, address spender) external returns (uint256);
+}
