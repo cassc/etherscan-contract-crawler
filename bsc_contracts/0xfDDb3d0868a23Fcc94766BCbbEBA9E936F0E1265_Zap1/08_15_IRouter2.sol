82ae1d2b63eb4851a4500bd5375dd50706143e9d
Chen <chencassc@gmail.com>
2022-11-27 12:42:42 +0800

    update contracts

---
+++ bsc_contracts/0xfDDb3d0868a23Fcc94766BCbbEBA9E936F0E1265_Zap1/08_15_IRouter2.sol
@@ -0,0 +1,61 @@
+/**
+ * @title Interface Router
+ * @dev IRouter2 contract
+ *
+ * @author - <USDFI TRUST>
+ * for the USDFI Trust
+ *
+ * SPDX-License-Identifier: GNU GPLv2
+ *
+ **/
+
+import "./IRouter1.sol";
+
+pragma solidity 0.6.12;
+
+interface IRouter2 is IRouter1 {
+    function removeLiquidityETHSupportingFeeOnTransferTokens(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountETH);
+
+    function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint256 amountETH);
+
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+
+    function swapExactETHForTokensSupportingFeeOnTransferTokens(
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable;
+
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+}
