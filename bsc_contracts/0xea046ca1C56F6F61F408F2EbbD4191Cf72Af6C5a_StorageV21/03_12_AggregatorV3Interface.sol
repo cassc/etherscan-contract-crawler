6d5f1ed3628bd628877a5cc25063fb6fc358f1aa
Chen <chencassc@gmail.com>
2022-12-07 12:40:59 +0800

    update contracts

---
+++ bsc_contracts/0xea046ca1C56F6F61F408F2EbbD4191Cf72Af6C5a_StorageV21/03_12_AggregatorV3Interface.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+
+    function latestAnswer() external view returns (int256 answer);
+}
