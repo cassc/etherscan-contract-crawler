f4abe8c23802039c4c6a07b9ad8e8f111ebe3351
Chen <chencassc@gmail.com>
2022-12-19 12:41:12 +0800

    update contracts

---
+++ bsc_contracts/0xB61Dab5e6dccf0D7d7E6b1394B1b660479870Adb_Basketv3/05_11_IWETH.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9 <0.9.0;
+
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint256 wad) external;
+}
