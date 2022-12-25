87730ec22cc09797d50d3f734ac1dc8118e916cb
Chen <chencassc@gmail.com>
2022-12-25 12:41:41 +0800

    update contracts

---
+++ bsc_contracts/0x2eb2C572EB4B85cF642b99F70d835C9C22346c06_PositionManager/15_17_ITimelock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+interface ITimelock {
+    function setAdmin(address _admin) external;
+    function enableLeverage(address _vault) external;
+    function disableLeverage(address _vault) external;
+    function setIsLeverageEnabled(address _vault, bool _isLeverageEnabled) external;
+    function signalSetGov(address _target, address _gov) external;
+}
