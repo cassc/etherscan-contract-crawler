3995e471d0e43860cb45d238dd999e43b03d6acf
Chen <chencassc@gmail.com>
2023-01-09 12:44:39 +0800

    update contracts

---
+++ bsc_contracts/0x091c7c0760265af58e8b1db8ea2e76b6fe0c3f9d_JcmLocked/03_03_Ownable.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Ownable {
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  } 
+}
---
+++ bsc_contracts/0x77e3a6e11b83a48382acecc86c07b35d34ef216e_JustClickMeToken/02_07_Context.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
