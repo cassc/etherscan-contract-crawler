e47f5c1050628f5bd96b6012ccd93f2194dbd2da
Chen <chencassc@gmail.com>
2023-02-23 14:37:24 +0800

    merge update from main

--- contract_crawler.py
+++ contract_crawler.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import json
-from typing import Optional, Dict, List
+from typing import Optional, Dict, List, Any
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
@@ -24,6 +24,9 @@ INPAGE_META_TEXT = {'Contract Name:': 'contract_name',
                     'Optimization Enabled': 'optimizations',
                     'Other Settings:': 'settings'}
 
+def address_from_tr(td: Any) -> str:
+    a = td.select_one('a.js-clipboard')
+    return a.attrs.get('data-clipboard-text') if (a and a.attrs) else None
 
 # Crawl meta info of contracts by page
 def parse_page(page: Optional[int]=None, retry=3, retry_delay=5) -> Optional[List[Dict[str, str]]]:
@@ -37,7 +40,7 @@ def parse_page(page: Optional[int]=None, retry=3, retry_delay=5) -> Optional[Lis
         soup = BeautifulSoup(resp.content, 'lxml')
         trs = soup.select('tr')
         table_headers = [th.text.strip() for th in trs[0].select('th')]
-        return [dict(zip(table_headers, [td.text.strip() for td in tr.select('td')])) for tr in trs[1:]]
+        return [dict(zip(table_headers, [address_from_tr(td) or td.text.strip() for td in tr.select('td')])) for tr in trs[1:]]
     except Exception as e:
         print(f'Error {e}')
         if retry > 0:
@@ -47,7 +50,7 @@ def parse_page(page: Optional[int]=None, retry=3, retry_delay=5) -> Optional[Lis
         else:
             raise e
 
-# Parse meta data from source code page         
+# Parse meta data from source code page
 def parse_for_inpage_meta(soup):
     rows = [t.text.strip().split('\n', maxsplit=1) for t in soup.select('#ContentPlaceHolder1_contractCodeDiv .row div')]
     # rows = [t.text.strip().split('\n+', maxsplit=1) for t in soup.select('#ContentPlaceHolder1_contractCodeDiv .row div')]
@@ -84,11 +87,12 @@ def parse_source_soup(soup, address=None, contract_name=None):
         with open(f, 'w') as f:
             f.write(source_code)
 
-    files =  [parse_for_file_name(name.text) for name in soup.select('.d-flex > .text-secondary') if '.sol' in name.text.strip()]
+    file_spans = [name for name in soup.select('.d-flex > .text-secondary') if '.sol' in name.text] or [name for name in soup.select('.d-flex > .text-muted') if '.sol' in name.text]
+    files =  [parse_for_file_name(name.text) for name in file_spans]
     sources = [source.text for source in soup.select('.js-sourcecopyarea')]
 
     if len(sources) != len(files): # some bscscan contracts have different DOM structure
-        sources = [source.text for source in soup.select('pre.editor')] 
+        sources = [source.text for source in soup.select('pre.editor')]
 
     inpage_meta = parse_for_inpage_meta(soup)
     write_source_file(f'inpage_meta.json', json.dumps(inpage_meta))
@@ -139,13 +143,13 @@ def download_source(contract: Dict[str, str], retry=3, retry_delay=5, throw_if_f
 
     if resp.status_code != 200:
         maybe_retry()
-                
+
     try:
         soup = BeautifulSoup(resp.content, 'lxml')
         parse_source_soup(soup, address, contract_name)
     except Exception as e:
         maybe_retry(e)
-    
+
 def fetch_all():
     contracts = [c for p in range(1, 21) for c in parse_page(p)]
     now = f"{datetime.now().strftime('%Y%m%d_%H%M%S')}"
@@ -161,7 +165,7 @@ def download_url_poly(url, retry=3, retry_delay=5, throw_if_fail=False):
     address = url.split('/')[-1].split('#')[0]
     driver = uc.Chrome()
     driver.get(url)
-    
+
     # fullscreen_btn = driver.find_elements(By.XPATH, '//a[@class="btn btn-xss btn-secondary togglefullscreen"]')
     # for btn in fullscreen_btn:
     #     btn.click()
@@ -170,8 +174,8 @@ def download_url_poly(url, retry=3, retry_delay=5, throw_if_fail=False):
     cookie = driver.get_cookies()[0]
     for key, value in cookie.items():
         cookie[key] = str(value)
-     
-    resp = requests.get(url, headers=REQ_HEADER, allow_redirects=True, cookies=cookie)    
+
+    resp = requests.get(url, headers=REQ_HEADER, allow_redirects=True, cookies=cookie)
     soup = BeautifulSoup(resp.content, 'lxml')
     parse_source_soup(soup, address)
 
@@ -217,7 +221,7 @@ if __name__ == '__main__':
         os.makedirs(ROOT_DIR, exist_ok=True)
         fn = download_url
 
-    elif web == "polygon": 
+    elif web == "polygon":
         VERIFIED_CONTRACT_URL = 'https://polygonscan.com/contractsVerified'
         CONTRACT_SOURCE_URL   = 'https://polygonscan.com/address/{}#code'
         ROOT_DIR = f'{OUTPUT_DIR}/polygon_contracts'
@@ -235,5 +239,5 @@ if __name__ == '__main__':
         fn(url)
     else:
         fetch_all()
-    
+
     print("all jobs done")
